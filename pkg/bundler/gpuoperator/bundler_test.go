e3f0360988dd89255a44372737cc272ab7495c17
Mark Chmarny <mchmarny@nvidia.com>
2025-12-28 12:28:45 -0800

    chore: remove old validation method

--- pkg/bundler/gpuoperator/bundler_test.go
+++ pkg/bundler/gpuoperator/bundler_test.go
@@ -90,9 +90,9 @@ func TestBundler_Validate(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := tt.recipe.Validate()
+			err := tt.recipe.ValidateMeasurementExists(measurement.TypeK8s)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("Validate() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("ValidateMeasurementExists() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
